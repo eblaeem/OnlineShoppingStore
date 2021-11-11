061205253626f3654f7a9893ca050bd4f9db2405
ebrahim ebrahimi <eblaeem@yahoo.com>
2021-11-11 12:51:32 +0330

    Finish Create DataBase

--- OnlineShoppingStore.Domain/Entities/User/UserRole.cs
+++ OnlineShoppingStore.Domain/Entities/User/UserRole.cs
@@ -2,7 +2,11 @@
 {
     public class UserRole
     {
+        public long Id { get; set; }
         public virtual User User { get; set; }
+        public long UserId { get; set; }
+
         public virtual Role Role { get; set; }
+        public long RoleId { get; set; }
     }
 }
